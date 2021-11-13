f1aa484cc9fe9e45d87d1663bddd9f5fd1f100f4
Magnus Groß <magnus.gross@rwth-aachen.de>
2021-11-13 22:25:58 +0100

    Catch filesystem errors
    It's okay to still add the path to the model. Just print a little
    warning and keep this->exists = false.

--- src/path.cpp
+++ src/path.cpp
@@ -1,5 +1,9 @@
 #include "path.hpp"
 
-Path::Path(std::string p) : path(std::filesystem::absolute(p)) {
-	exists = std::filesystem::exists(path);
+Path::Path(const std::string& p) : path(std::filesystem::absolute(p)) {
+	try {
+		exists = std::filesystem::exists(path);
+	} catch(const std::filesystem::filesystem_error& ex) {
+		std::cerr << ex.what() << std::endl;
+	}
 }
--- src/path.hpp
+++ src/path.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <iostream>
 #include <filesystem>
 #include <QUrl>
 
 class Path {
 public:
-	Path(std::string p);
+	Path(const std::string& p);
 	std::filesystem::path path;
 	bool used = false;
 	QUrl thumbnail;
