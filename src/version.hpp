758606d04f0b903417c26da79c27b696c2212573
Magnus Groß <magnus@mggross.com>
2023-07-08 19:55:04 +0200

    Remove fallback version number
    It's better to have the version in just one place (in cmake).

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,6 @@
 
 namespace Version {
 
-#ifndef BLOBDROP_VERSION
-// fallback version, if cmake failed to set the version
-#define BLOBDROP_VERSION "1.0"
-#endif
-
 const char *version_string();
 
 }
