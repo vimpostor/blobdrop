d5bf811a19c78c2708377ee752d89e5c018ff6f8
Magnus Groß <magnus@mggross.com>
2023-07-15 22:40:09 +0200

    Add mimetype support for directories
    QMimeType does not seem to handle them correctly.

--- src/mimedb.cpp
+++ src/mimedb.cpp
@@ -1,6 +1,14 @@
 #include "mimedb.hpp"
 
 std::string MimeDb::getIcon(const std::filesystem::path &p) const {
+	if (std::filesystem::is_directory(p)) {
+		/**
+		 * Early return for directories:
+		 * mimeTypeForFile incorrectly returns application-octet-stream for them
+		*/
+		return "inode-directory";
+	}
+
 	std::string result = getMimetype(p).iconName().toStdString();
 	if (result.empty()) {
 		result = "text-x-generic";
