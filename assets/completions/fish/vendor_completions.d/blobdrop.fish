7932dca44e434041f6755a7cb85d86d587596c5c
Magnus Groß <magnus@mggross.com>
2023-08-03 23:47:07 +0200

    Add fish completions
    refs: https://fishshell.com/docs/current/completions.html

---
+++ assets/completions/fish/vendor_completions.d/blobdrop.fish
@@ -0,0 +1,9 @@
+complete -c blobdrop -s '-h' -l 'help' -d 'show help'
+complete -c blobdrop -s '-v' -l 'version' -d 'show version'
+complete -c blobdrop -s '-f' -l 'frameless' -d 'show frameless window'
+complete -c blobdrop -s '-k' -l 'keep' -d 'keep dropped files'
+complete -c blobdrop -s '-l' -l 'link' -d 'print OSC8 hyperlinks'
+complete -c blobdrop -s '-n' -l 'notification' -d 'send notification'
+complete -c blobdrop -s '-p' -l 'persistent' -d 'disable autohiding during drag'
+complete -c blobdrop -s '-t' -l 'ontop' -d 'keep window on top'
+complete -c blobdrop -s '-x' -l 'auto-quit' -d 'autoquit behaviour' -x -a "0\t'do not autoquit' 1\t'after first drag' 2\t'after all items have been dragged'"
