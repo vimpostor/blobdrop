456142454fbdea1ae02737408726278aa32cbc5e
Magnus Groß <magnus@mggross.com>
2023-09-28 00:01:27 +0200

    Don't show plural title for a single file

--- src/backend.cpp
+++ src/backend.cpp
@@ -14,6 +14,10 @@ void Backend::send_drag_notification(const QList<QString> &uris) {
 	auto msg = QDBusMessage::createMethodCall("org.freedesktop.Notifications", "/org/freedesktop/Notifications", "org.freedesktop.Notifications", "Notify");
 	QVariantMap hints;
 	hints["x-kde-urls"] = uris;
-	msg.setArguments({"blobdrop", 0U, "cursor-arrow", "Drag files", "", QStringList(), hints, 5000});
+	QString title = "Drag file";
+	if (uris.size() > 1) {
+		title += "s";
+	}
+	msg.setArguments({"blobdrop", 0U, "cursor-arrow", title, "", QStringList(), hints, 5000});
 	session.call(msg);
 }
