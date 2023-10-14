31b32f8e5744e2994496b1699609ac26eda8a463
Magnus Groß <magnus@mggross.com>
2023-10-14 16:56:58 +0200

    Remove deprecated frontend options

--- src/getopts.cpp
+++ src/getopts.cpp
@@ -18,18 +18,9 @@ bool parse(QCoreApplication &app) {
 												  << "frontend",
 		"Selects the frontend. Must be one of {auto (default), gui, immediate, notify, link}",
 		"frontend");
-	QCommandLineOption immediate_opt(QStringList() << "i"
-												   << "immediate",
-		"Drag immediately.");
 	QCommandLineOption keep_opt(QStringList() << "k"
 											  << "keep",
 		"Keep dropped files around in sink mode.");
-	QCommandLineOption link_opt(QStringList() << "l"
-											  << "link",
-		"Print paths to stdout via OSC8 hyperlinks.");
-	QCommandLineOption notify_opt(QStringList() << "n"
-												<< "notification",
-		"Send a notification for dragging.");
 	QCommandLineOption persistent_opt(QStringList() << "p"
 													<< "persistent",
 		"Do not auto-hide the main window while dragging.");
@@ -41,11 +32,7 @@ bool parse(QCoreApplication &app) {
 		"The amount of drags after which the program should automatically close. Must be one of {never, first, all (default)}",
 		"number");
 
-	// hide deprecated legacy options from help
-	const std::array hidden_options = {&immediate_opt, &link_opt, &notify_opt};
-	std::ranges::for_each(hidden_options, [](auto o) { o->setFlags(QCommandLineOption::Flag::HiddenFromHelp); });
-
-	p.addOptions({frameless_opt, frontend_opt, immediate_opt, keep_opt, link_opt, notify_opt, persistent_opt, ontop_opt, auto_quit_opt});
+	p.addOptions({frameless_opt, frontend_opt, keep_opt, persistent_opt, ontop_opt, auto_quit_opt});
 	p.process(app);
 
 	if (p.isSet(auto_quit_opt)) {
@@ -75,20 +62,6 @@ bool parse(QCoreApplication &app) {
 		Settings::get()->keep_dropped_files = true;
 	}
 
-	bool used_deprecated_frontend_syntax = true;
-	if (p.isSet(immediate_opt)) {
-		Settings::get()->frontend = Settings::Frontend::Immediate;
-	} else if (p.isSet(link_opt)) {
-		Settings::get()->frontend = Settings::Frontend::Link;
-	} else if (p.isSet(notify_opt)) {
-		Settings::get()->frontend = Settings::Frontend::Notification;
-	} else {
-		used_deprecated_frontend_syntax = false;
-	}
-	if (used_deprecated_frontend_syntax) {
-		std::cerr << "This option for choosing the frontend is deprecated and will be removed in a future release." << std::endl
-				  << "Please use the --frontend option instead." << std::endl;
-	}
 	if (p.isSet(frontend_opt)) {
 		constexpr std::array str_repr = {"auto", "gui", "immediate", "notify", "link"};
 		int choice = std::ranges::find(str_repr, p.value(frontend_opt).toStdString()) - str_repr.cbegin();
