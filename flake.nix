2cfe30df66df7b2eeddba5db974e07cd1a64cf50
Magnus Groß <magnus@mggross.com>
2023-11-05 01:32:48 +0100

    nix: Add an option to compile with clang
    The compiler toolchain can now be chosen as a target, e.g. to use clang:
    
    nix run './.#clang'
    
    By default gcc is used.
    Note that the clang build is broken right now, because nixpkgs does not
    ship LLVM17 yet.

--- flake.nix
+++ flake.nix
@@ -8,31 +8,32 @@
 	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
 		let
 			pkgs = nixpkgs.legacyPackages.${system};
+			stdenvs = [ { name = "gcc"; pkg = pkgs.gcc13Stdenv; } { name = "clang"; pkg = pkgs.llvmPackages_16.stdenv; } ];
+			defaultStdenv = (builtins.head stdenvs).name;
+			makeStdenvPkg = env: env.mkDerivation {
+				pname = "blobdrop";
+				version = "2.0";
+
+				src = ./.;
+
+				nativeBuildInputs = with pkgs; [
+					cmake
+					pkg-config
+					qt6.wrapQtAppsHook
+				];
+
+				buildInputs = with pkgs; [
+					qt6.qtbase
+					qt6.qtdeclarative
+					qt6.qtsvg
+					xorg.libxcb
+					xorg.xcbutilwm
+				];
+			};
 		in {
 			packages = rec {
-				default = blobdrop;
-
-				blobdrop = pkgs.gcc13Stdenv.mkDerivation {
-					pname = "blobdrop";
-					version = "2.0";
-
-					src = ./.;
-
-					nativeBuildInputs = with pkgs; [
-						cmake
-						pkg-config
-						qt6.wrapQtAppsHook
-					];
-
-					buildInputs = with pkgs; [
-						qt6.qtbase
-						qt6.qtdeclarative
-						qt6.qtsvg
-						xorg.libxcb
-						xorg.xcbutilwm
-					];
-				 };
-			};
+				default = self.outputs.packages.${system}.${defaultStdenv};
+			} // builtins.listToAttrs (map (x: { name = x.name; value = makeStdenvPkg x.pkg; }) stdenvs);
 		}
 	);
 }
