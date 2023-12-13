vimpostor/blobdrop

commit 47bb2fcd35a254dc3619ae0e2136f7be70e905b6

Refactor low-level xcb code into separate class
This simplifies things a lot on the caller side.

src/xcb.cpp
@@ -1,0 +1,56 @@
+#include "xcb.hpp"
+
+#include <QGuiApplication>
+#include <xcb/xcb.h>
+
+bool Xcb::init() {
+	if (ok) {
+		// don't need to init twice
+		return true;
+	}
+
+	const auto qt_x11 = qGuiApp->nativeInterface<QNativeInterface::QX11Application>();
+	if (!qt_x11) {
+		return false;
+	}
+	// reuse the Qt xcb connection
+	conn = qt_x11->connection();
+	if (!conn) {
+		return false;
+	}
+
+	// init ewmh connection
+	ok = xcb_ewmh_init_atoms_replies(&ewmh, xcb_ewmh_init_atoms(conn, &ewmh), nullptr);
+	return ok;
+}
+
+xcb_window_t Xcb::active_window() {
+	xcb_window_t res;
+	// try to get the currently active/focused window
+	// Notice that we pass 0 for screen_nbr, which is not quite correct.
+	// We should pass the default screen number instead, which we can obtain via the second parameter to xcb_connect().
+	// https://xcb.freedesktop.org/xlibtoxcbtranslationguide/
+	// Unfortunately, we don't have that as we just grab the connection from Qt.
+	//
+	// Apparently previously Qt could return the default screen number with QX11Info::appScreen(),
+	// but that is not available in Qt6 anymore.
+	if (!xcb_ewmh_get_active_window_reply(&ewmh, xcb_ewmh_get_active_window_unchecked(&ewmh, 0), &res, nullptr)) {
+		qWarning() << "Cannot get active window";
+		return 0;
+	}
+	return res;
+}
+
+void Xcb::set_keep_window_below(xcb_window_t window, const bool value) {
+	const auto v = value ? XCB_EWMH_WM_STATE_ADD : XCB_EWMH_WM_STATE_REMOVE;
+
+	// add the corresponding enum value to the window's _NET_WM_STATE
+	// this method can set two values at once (e.g. to maximize both vertically and horizontally in one call),
+	// but we don't need that, so we set the second parameter to XCB_ATOM_NONE
+	std::ignore = xcb_ewmh_request_change_wm_state(&ewmh, 0, window, v, ewmh._NET_WM_STATE_BELOW, XCB_ATOM_NONE, XCB_EWMH_CLIENT_SOURCE_TYPE_NORMAL);
+	// this is needed so that the call actually takes effect
+	std::ignore = xcb_get_window_attributes_reply(conn, xcb_get_window_attributes(conn, window), nullptr);
+
+	// write pending data to the socket
+	xcb_flush(conn);
+}

src/backend.cpp
@@ -2,8 +2,6 @@
 
 #include <QDBusInterface>
 #include <QGuiApplication>
-#include <xcb/xcb.h>
-#include <xcb/xcb_ewmh.h>
 
 #include "settings.hpp"
 
@@ -26,28 +24,13 @@ void Backend::send_drag_notification(const QList<QString> &uris) {
 }
 
 void Backend::hide_terminal() {
-	if (Settings::get()->supress_always_on_bottom) {
+	if (Settings::get()->supress_always_on_bottom || !xcb.init()) {
 		return;
 	}
 
-	const auto qt_x11 = qGuiApp->nativeInterface<QNativeInterface::QX11Application>();
-	if (!qt_x11) {
-		return;
-	}
-	auto *xcb_conn = qt_x11->connection();
-
-	xcb_ewmh_connection_t ewmh;
-	xcb_ewmh_init_atoms(xcb_conn, &ewmh);
-	if (!xcb_ewmh_init_atoms_replies(&ewmh, xcb_ewmh_init_atoms(xcb_conn, &ewmh), nullptr)) {
-		qWarning() << "Cannot connect to ewmh";
-	} else {
-		if (!xcb_ewmh_get_active_window_reply(&ewmh, xcb_ewmh_get_active_window_unchecked(&ewmh, 0), &last_window, nullptr)) {
-			qWarning() << "Cannot get active window";
-		} else {
-			std::ignore = xcb_ewmh_request_change_wm_state(&ewmh, 0, last_window, XCB_EWMH_WM_STATE_ADD, ewmh._NET_WM_STATE_BELOW, XCB_ATOM_NONE, XCB_EWMH_CLIENT_SOURCE_TYPE_NORMAL);
-			std::ignore = xcb_get_window_attributes_reply(xcb_conn, xcb_get_window_attributes(xcb_conn, last_window), nullptr);
-			xcb_flush(xcb_conn);
-		}
+	last_window = xcb.active_window();
+	if (last_window) {
+		xcb.set_keep_window_below(last_window);
 	}
 }
 
@@ -56,19 +39,6 @@ void Backend::restore_terminal() {
 		return;
 	}
 
-	const auto qt_x11 = qGuiApp->nativeInterface<QNativeInterface::QX11Application>();
-	if (!qt_x11) {
-		return;
-	}
-	auto *xcb_conn = qt_x11->connection();
-
-	xcb_ewmh_connection_t ewmh;
-	xcb_ewmh_init_atoms(xcb_conn, &ewmh);
-	if (!xcb_ewmh_init_atoms_replies(&ewmh, xcb_ewmh_init_atoms(xcb_conn, &ewmh), nullptr)) {
-		qWarning() << "Cannot connect to ewmh";
-	} else {
-		std::ignore = xcb_ewmh_request_change_wm_state(&ewmh, 0, last_window, XCB_EWMH_WM_STATE_REMOVE, ewmh._NET_WM_STATE_BELOW, XCB_ATOM_NONE, XCB_EWMH_CLIENT_SOURCE_TYPE_NORMAL);
-		std::ignore = xcb_get_window_attributes_reply(xcb_conn, xcb_get_window_attributes(xcb_conn, last_window), nullptr);
-		xcb_flush(xcb_conn);
-	}
+	xcb.set_keep_window_below(last_window, false);
+	last_window = 0;
 }

src/backend.hpp
@@ -2,9 +2,9 @@
 
 #include <QCoreApplication>
 #include <QTimer>
-#include <xcb/xproto.h>
 
 #include "Util/util.hpp"
+#include "xcb.hpp"
 
 class Backend {
 public:
@@ -16,5 +16,6 @@ public:
 	void hide_terminal();
 	void restore_terminal();
 private:
+	Xcb xcb;
 	xcb_window_t last_window = 0;
 };

src/xcb.hpp
@@ -1,0 +1,15 @@
+#pragma once
+
+#include <xcb/xcb_ewmh.h>
+#include <xcb/xproto.h>
+
+class Xcb {
+public:
+	bool init();
+	xcb_window_t active_window();
+	void set_keep_window_below(xcb_window_t window, const bool value = true);
+private:
+	bool ok = false;
+	xcb_connection_t *conn = nullptr;
+	xcb_ewmh_connection_t ewmh;
+};

commit 5db8f0a5dce6bd0db2091dcb266e68fdf47d5448

Do not init xcb twice
Regression was introduced in 47bb2fcd.

src/xcb.cpp
@@ -37,7 +37,8 @@ bool Xcb::init() {
 	}
 
 	// init ewmh connection
-	return xcb_ewmh_init_atoms_replies(&ewmh, xcb_ewmh_init_atoms(conn, &ewmh), nullptr);
+	ok = xcb_ewmh_init_atoms_replies(&ewmh, xcb_ewmh_init_atoms(conn, &ewmh), nullptr);
+	return ok;
 }
 
 xcb_window_t Xcb::active_window() {